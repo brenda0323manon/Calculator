60884a4a5056fc47bba928406e2277bed20e043a
brendamanon0526@gmail <Brenda_Manon@adminisatorsmbp.attlocal.net>
2023-10-19 22:28:37 -0400

    calculator

---
+++ script.js
@@ -0,0 +1,39 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display'); 
+    display.value = '';
+}
+
+// // function percentageDisplay(num,per) {
+//        return (num/100)*performance;
+// //     const display = document.getElementById('display');
+// //     display.value = value;
+// }
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    }   catch (error) {
+        display.value = 'Error';
+    }
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach ((button) => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'C') {
+            clearDisplay();
+        }   else if (button.textContent === '=') {
+            calculate();
+        }   else {
+            appendToDisplay(button.textContent);
+        }
+    });
+});
+
