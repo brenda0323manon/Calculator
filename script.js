dc04a4ccc47cf167ee2fdd43f8352cac056ce1fb
brendamanon0526@gmail <Brenda_Manon@adminisatorsmbp.attlocal.net>
2023-10-20 14:59:50 -0400

    fixed layout

--- script.js
+++ script.js
@@ -15,15 +15,13 @@ function calculate() {
             const percentage = parseFloat(display.value) / 100;
             display.value = percentage;
     }   else {
-        display.value = eval(display.value);
-    }
+        display.value = eval(display.value);}
     }   catch (error) {
         display.value = 'Error';
     }
 }
 
 const buttons = document.querySelectorAll('button');
-
 buttons.forEach ((button) => {
     button.addEventListener('click', () => {
         if (button.textContent === 'C') {
